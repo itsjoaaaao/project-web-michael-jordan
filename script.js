6c016de793eff36546d614faaf2b1f205d49cb0d
João Paulo <87874652+itsjoaaaao@users.noreply.github.com>
2023-01-24 15:36:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.alert('Seja Bem Vindo!!')
+
+var a = window.document.getElementById('titulo')
+
+function mudar(){
+    a.innerHTML = `The GOAT!\u{1F410}`
+}
+
+function voltar(){
+    a.innerText = "Michael Jordan"
+}
